251570ec40ec542d6caefb75a6e679dc2d15bc97
Miguel De Anda <miguel.deanda@jamcity.com>
2020-07-31 15:12:18 -0700

    adds bucket for crm to run from the same image

--- couchbase-fast/setup.sh
+++ couchbase-fast/setup.sh
@@ -13,18 +13,21 @@ done
 
 couchbase-cli cluster-init --cluster-username=gs --cluster-password=admin123 --cluster-port=8091 --cluster-ramsize=1024 --cluster-index-ramsize=1024 --services=data,index,query
 
-couchbase-cli bucket-create -c localhost --bucket=gs --bucket-type=couchbase --bucket-ramsize=512 -u gs -p admin123
-couchbase-cli bucket-create -c localhost --bucket=edms --bucket-type=couchbase --bucket-ramsize=256 -u gs -p admin123
-couchbase-cli bucket-create -c localhost --bucket=admin-new --bucket-type=couchbase --bucket-ramsize=256 -u gs -p admin123
+couchbase-cli bucket-create -c localhost --bucket=gs --bucket-type=couchbase --bucket-ramsize=100 -u gs -p admin123
+couchbase-cli bucket-create -c localhost --bucket=edms --bucket-type=couchbase --bucket-ramsize=100 -u gs -p admin123
+couchbase-cli bucket-create -c localhost --bucket=admin-new --bucket-type=couchbase --bucket-ramsize=100 -u gs -p admin123
+couchbase-cli bucket-create -c localhost --bucket=data --bucket-type=couchbase --bucket-ramsize=100 -u gs -p admin123
 
 sleep 10
 
 cbq -e http://localhost:8093 -u gs -p admin123 -s "CREATE PRIMARY INDEX ON \`gs\`;"
 cbq -e http://localhost:8093 -u gs -p admin123 -s "CREATE PRIMARY INDEX ON \`edms\`;"
 cbq -e http://localhost:8093 -u gs -p admin123 -s "CREATE PRIMARY INDEX ON \`admin-new\`;"
+cbq -e http://localhost:8093 -u gs -p admin123 -s "CREATE PRIMARY INDEX ON \`data\`;"
 
 sleep 5
 
 couchbase-cli user-manage -c localhost -u gs -p admin123 --set --rbac-username gs --rbac-password admin123 --roles admin --auth-domain local
 couchbase-cli user-manage -c localhost -u gs -p admin123 --set --rbac-username edms --rbac-password admin123 --roles admin --auth-domain local
 couchbase-cli user-manage -c localhost -u gs -p admin123 --set --rbac-username admin-new --rbac-password admin123 --roles admin --auth-domain local
+couchbase-cli user-manage -c localhost -u gs -p admin123 --set --rbac-username data --rbac-password admin123 --roles admin --auth-domain local
